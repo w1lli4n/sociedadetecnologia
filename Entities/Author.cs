bee7ee90bb62c68428580f643a5c2141d6b80bcb
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2024-05-27 17:35:14 -0300

    (feat) Adicionada a base para a inserção dos artigos

---
+++ Entities/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace SociedadeTecnologia.Entities;
+
+public class Author(string? name, string? about, string? avatarPath)
+{
+    public string? Name { get; set; } = name;
+    public string? About { get; set; } = about;
+    public string? AvatarPath { get; set; } = avatarPath;
+}
