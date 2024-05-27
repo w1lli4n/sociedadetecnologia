16663f2e85d70b64056cdc88b64a82528dff4527
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2024-05-27 18:05:12 -0300

    (feat) Adcionada a base da página Sobre

--- Entities/Author.cs
+++ Entities/Author.cs
@@ -1,8 +1,11 @@
 ﻿namespace SociedadeTecnologia.Entities;
 
-public class Author(string? name, string? about, string? avatarPath)
+public class Author(string? name = "", string? about = "", string? description = "", string? avatarPath = "", string? imagePath = "")
 {
     public string? Name { get; set; } = name;
     public string? About { get; set; } = about;
+    public string? Description { get; set; } = description;
     public string? AvatarPath { get; set; } = avatarPath;
+    public string? ImagePath { get; set; } = imagePath;
+
 }
