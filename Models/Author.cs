59b9761eb12a8b0c3d7de960aaba968cb704ef46
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2024-05-28 15:15:47 -0300

    (refactor) Refatorado alguns componentes, especialmente nos estilos e apresentação.

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,11 +1,9 @@
 ﻿namespace SociedadeTecnologia.Models;
 
-public class Author(string? name = "", string? about = "", string? description = "", string? avatarPath = "", string? imagePath = "")
+public class Author(string name, string about, string avatarPath, string imagePath)
 {
-    public string? Name { get; set; } = name;
-    public string? About { get; set; } = about;
-    public string? Description { get; set; } = description;
-    public string? AvatarPath { get; set; } = avatarPath;
-    public string? ImagePath { get; set; } = imagePath;
-
+    public string Name { get; set; } = name;
+    public string About { get; set; } = about;
+    public string AvatarPath { get; set; } = avatarPath;
+    public string ImagePath { get; set; } = imagePath;
 }
