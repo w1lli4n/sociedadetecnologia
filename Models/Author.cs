bff713b63d9805a6f52027ca175f76e13d375ca7
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2024-06-05 15:25:49 -0300

    (fuzzy) Refatorado o código e finalizado o projeto

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,9 +1,7 @@
 ﻿namespace SociedadeTecnologia.Models;
 
-public class Author(string name, string about, string avatarPath, string imagePath)
+public class Author(string name, string about)
 {
     public string Name { get; set; } = name;
     public string About { get; set; } = about;
-    public string AvatarPath { get; set; } = avatarPath;
-    public string ImagePath { get; set; } = imagePath;
 }
